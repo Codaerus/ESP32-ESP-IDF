7efde5c3a32c198970e79e998bf5c6acdc0008dc
Codaerus <christianqc91@gmail.com>
2025-05-31 12:32:24 -0500

    Create ejemplo7.c

---
+++ CLASE1/ejemplo7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Humano{
+    int edad;
+    char nombre[30];
+    union{
+        char telefono[15];
+        char correo[40];
+    }contacto;
+    struct {
+        int matricula;
+        float promedio;
+    } e;
+}Humano;
+//typedef struct Humano Humano;
+
+void imprimirDatos(struct Humano *p){
+    printf("El nombre es %s\n", p->nombre);
+    printf("La edad es %d\n", p->edad);
+    printf("El codigo de alumno es %d y su promedio es %.1f\n",p->e.matricula,p->e.promedio);
+}
+Humano crearHumano(int e, char *n, int m, float p, char *t){
+    Humano h;
+    h.edad = e;
+    strcpy(h.nombre,n);
+    h.e.matricula = m;
+    h.e.promedio = p;
+    strcpy(h.contacto.telefono,t);
+    return h;
+}
+void main(){
+    Humano juan = crearHumano(27,"Juan",1234,14.0,"925147621");
+    imprimirDatos(&juan);
+    printf("El telefono es %s\n",juan.contacto.telefono);
+    strcpy(juan.contacto.correo,"juan27@gmail.com");
+    printf("El telefono es %s\n",juan.contacto.telefono);
+    return;
+}    
